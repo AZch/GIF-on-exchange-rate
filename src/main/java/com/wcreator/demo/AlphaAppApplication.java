05f40d95aa65ae1aa2a058a81237db59a6817834
AZchVVT <a.zhuykov@velvetech.ru>
2021-01-20 19:32:27 +0700

    init

---
+++ src/main/java/com/wcreator/demo/AlphaAppApplication.java
@@ -0,0 +1,13 @@
+package com.wcreator.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wcreator/demo/AlphaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcreator.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
