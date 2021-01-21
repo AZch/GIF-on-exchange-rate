725c157d3017f1fe1a7fdb508b9e64293eed9166
AZchVVT <a.zhuykov@velvetech.ru>
2021-01-21 12:49:37 +0700

    implement service for compare rates

--- src/main/java/com/wcreator/demo/clients/exchange/configuration/ExchangeConfiguration.java
+++ src/main/java/com/wcreator/demo/clients/exchange/configuration/ExchangeConfiguration.java
@@ -1,12 +1,16 @@
 package com.wcreator.demo.clients.exchange.configuration;
 
-import com.wcreator.demo.clients.exchange.interceptor.ExchangeClientInterceptor;
+import com.wcreator.demo.clients.exchange.interceptor.ExchangeClientAuthInterceptor;
+import com.wcreator.demo.clients.exchange.interceptor.ExchangeClientCurrencyInterceptor;
 import org.springframework.context.annotation.Bean;
 
 public class ExchangeConfiguration {
 
     @Bean
-    public ExchangeClientInterceptor exchangeClientInterceptor() {
-        return new ExchangeClientInterceptor();
+    public ExchangeClientAuthInterceptor exchangeClientInterceptor() {
+        return new ExchangeClientAuthInterceptor();
     }
+
+    @Bean
+    public ExchangeClientCurrencyInterceptor exchangeClientCurrencyInterceptor() { return new ExchangeClientCurrencyInterceptor(); }
 }
